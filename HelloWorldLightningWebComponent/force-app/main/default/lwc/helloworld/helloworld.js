02f5bfc919c3c197a37bcfffe771b974667bbecd
Manuel Vargas <manuelvargas1251@gmail.com>
2021-11-04 12:48:36 -0500

    Update helloworld.js

--- HelloWorldLightningWebComponent/force-app/main/default/lwc/helloworld/helloworld.js
+++ HelloWorldLightningWebComponent/force-app/main/default/lwc/helloworld/helloworld.js
@@ -1,7 +1,15 @@
-import { LightningElement } from 'lwc';
+import {LightningElement,track} from 'lwc';
 export default class HelloWorld extends LightningElement {
-  greeting = 'World';
-  changeHandler(event) {
-    this.greeting = event.target.value;
-  }
+		@track BTCVALUE;
+		updateButton(){
+				const d = new Date().getTime()
+				console.log('test 🎃' + d);
+				fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+						.then(response => response.json())
+						.then(data => {
+						console.log(data.bpi.USD.rate_float);
+						this.BTCVALUE = data.bpi.USD.rate_float;
+				});
+				//console.log(data.bpi.USD.rate_float);
+		}
 }
